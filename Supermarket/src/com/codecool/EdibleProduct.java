c860e476ad00e36d64961bdcf42911f97af19515
Bernáth Máté <bernath.mate.new@gmail.com>
2018-01-17 16:42:45 +0100

    class fields and the constructore are done

--- Supermarket/src/com/codecool/EdibleProduct.java
+++ Supermarket/src/com/codecool/EdibleProduct.java
@@ -1,3 +1,11 @@
 public class EdibleProduct extends Product {
 
+    private int calorie;
+    private boolean isAlcohol;
+
+    public EdibleProduct(String name, int price, int calorie, boolean isAlcohol) {
+        super(name, price);
+        this.calorie = calorie;
+        this.isAlcohol = isAlcohol;
+    }
 }
