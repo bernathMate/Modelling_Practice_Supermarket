4a471e7377271a7c593a7fe93d33ebac374e25fe
Bernáth Máté <bernath.mate.new@gmail.com>
2018-01-17 15:58:22 +0100

    class fields and the constructor is done

--- Supermarket/src/com/codecool/Supermarket.java
+++ Supermarket/src/com/codecool/Supermarket.java
@@ -1,3 +1,11 @@
 public class Supermarket {
-    
+    private String name;
+    private Person[] persons;
+    private Product[] products;
+
+    public Supermarket(String name) {
+        this.name = name;
+        persons = new Person[0];
+        products = new Product[0];
+    } 
 }
