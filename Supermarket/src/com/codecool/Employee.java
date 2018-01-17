dd00147eb1fedbdb82de71ff9024b46b3bfa5e3a
Bernáth Máté <bernath.mate.new@gmail.com>
2018-01-17 17:16:38 +0100

    fix the constructor

--- Supermarket/src/com/codecool/Employee.java
+++ Supermarket/src/com/codecool/Employee.java
@@ -2,5 +2,10 @@ public class Employee extends Person {
     
     private String position;
 
-    public Employee(String fullName, String gender, int age, String position)
+    public Employee(String fullName, String gender, int age, String position) {
+        super(fullName, gender, age);
+        this.position = position;
+    }
+
+    
 }
