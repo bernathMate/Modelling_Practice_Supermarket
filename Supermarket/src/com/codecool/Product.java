7a90506f9b683db74ef77920230086936ba07d30
Bernáth Máté <bernath.mate.new@gmail.com>
2018-01-17 16:34:31 +0100

    getters are done

--- Supermarket/src/com/codecool/Product.java
+++ Supermarket/src/com/codecool/Product.java
@@ -7,5 +7,13 @@ public class Product {
         this.name = name;
         this.price = price;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
     
 }
