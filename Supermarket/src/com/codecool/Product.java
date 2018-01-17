9b200974e4ff9b6b22fa0042660e9eb0a434a76b
Bernáth Máté <bernath.mate.new@gmail.com>
2018-01-17 16:33:11 +0100

    class fields and the constructor is done

--- Supermarket/src/com/codecool/Product.java
+++ Supermarket/src/com/codecool/Product.java
@@ -1,3 +1,11 @@
 public class Product {
+
+    private String name;
+    private int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
     
 }
