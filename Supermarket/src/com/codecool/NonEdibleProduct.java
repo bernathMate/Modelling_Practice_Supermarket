ec3af6d72c780b3f31ec30b303ef65d4587cdcf0
Bernáth Máté <bernath.mate.new@gmail.com>
2018-01-17 16:52:31 +0100

    class field and constructore are done

--- Supermarket/src/com/codecool/NonEdibleProduct.java
+++ Supermarket/src/com/codecool/NonEdibleProduct.java
@@ -1,3 +1,10 @@
 public class NonEdibleProduct extends Product {
+
+    private String material;
+
+    public NonEdibleProduct(String name, int price, String material) {
+        super(name, price);
+        this.material = material;
+    }
     
 }
