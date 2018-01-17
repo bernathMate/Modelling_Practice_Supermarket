a10616d001a92c0838c71a1e6d79e34db8822f01
Bernáth Máté <bernath.mate.new@gmail.com>
2018-01-17 16:13:04 +0100

    class fields and the constructor is done

--- Supermarket/src/com/codecool/Person.java
+++ Supermarket/src/com/codecool/Person.java
@@ -1,3 +1,12 @@
 public class Person {
     
+    private String fullName;
+    private String gender;
+    private int age;
+
+    public Person(String fullName, String gender, int age) {
+        this.fullName = fullName;
+        this.gender = gender;
+        this.age = age;
+    }
 }
