ba9dcda924c72a76710b118dabd467f096e1c88d
Bernáth Máté <bernath.mate.new@gmail.com>
2018-01-19 10:28:40 +0100

    constructor is modified

--- Supermarket/src/com/codecool/Customer.java
+++ Supermarket/src/com/codecool/Customer.java
@@ -3,8 +3,8 @@ public class Customer extends Person {
     private int money;
     private Product[] shoppingCart;
 
-    public Customer(String fullName, String gender, int age, int money) {
-        super(fullName, gender, age);
+    public Customer(String fullName, String gender, int age, int money, PersonType personType) {
+        super(fullName, gender, age, personType);
         this.money = money;
         shoppingCart = new Product[30];
     }
--- Supermarket/src/com/codecool/Employee.java
+++ Supermarket/src/com/codecool/Employee.java
@@ -2,8 +2,8 @@ public class Employee extends Person {
     
     private String position;
 
-    public Employee(String fullName, String gender, int age, String position) {
-        super(fullName, gender, age);
+    public Employee(String fullName, String gender, int age, String position, PersonType personType) {
+        super(fullName, gender, age personType);
         this.position = position;
     }
 
