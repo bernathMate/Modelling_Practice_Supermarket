9b591771210a25acccb3049a2b46092ef8cb1993
Bernáth Máté <bernath.mate.new@gmail.com>
2018-01-19 10:11:51 +0100

    addToPerson, addToProduct functions are done

--- Supermarket/src/com/codecool/Supermarket.java
+++ Supermarket/src/com/codecool/Supermarket.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Supermarket {
     private String name;
     private Person[] persons;
@@ -7,6 +9,10 @@ public class Supermarket {
         this.name = name;
         persons = new Person[0];
         products = new Product[0];
+    }
+
+    public void init() {
+        System.out.println("Welcome in the " + getName() + " supermarket!");
     }
 
     public String getName() {
@@ -20,4 +26,24 @@ public class Supermarket {
     public Product[] getProducts() {
         return products;
     }
+
+    public void addToPerson(Person person) {
+        Person[] tempArray = new Person[persons.length + 1];
+        for(int i = 0; i < persons.length; i++) {
+            tempArray[i] = persons[i];
+        }
+        tempArray[tempArray.length - 1] = person;
+        persons = tempArray;
+    }
+
+    public void addToProduct(Product product) {
+        Product[] tempArray = new Product[products.length + 1];
+        for(int i = 0; i < products.length; i++) {
+            tempArray[i] = products[i];
+        }
+        tempArray[tempArray.length - 1] = product;
+        products = tempArray;
+    }
+
+    public Product createProduct(Pro)
 }
