ead30d8390522a5d4e4fc99a9bf2ae4f426f8792
Bernáth Máté <bernath.mate.new@gmail.com>
2018-01-20 15:27:06 +0100

    toString() method is added

--- Supermarket/src/com/codecool/Employee.java
+++ Supermarket/src/com/codecool/Employee.java
@@ -1,13 +1,24 @@
+package com.codecool;
+
 public class Employee extends Person {
     
     private String position;
 
     public Employee(String fullName, String gender, int age, String position, PersonType personType) {
-        super(fullName, gender, age personType);
+        super(fullName, gender, age, personType);
         this.position = position;
     }
 
     public String getPosition() {
         return position;
+    }
+
+    @Override
+    public String toString() {
+        return "Name= " + getFullName()
+                + ", Gender= " + getGender()
+                + ", Age= " + getAge()
+                + ", Position= " + getPosition()
+                + ", Person's type= " + getPersonType();
     }
 }
