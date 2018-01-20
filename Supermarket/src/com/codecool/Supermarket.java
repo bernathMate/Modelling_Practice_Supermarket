a6fe938e01d6c44d5a1d5319576c7c0c60ae155a
Bernáth Máté <bernath.mate.new@gmail.com>
2018-01-20 15:34:14 +0100

    createCustomer(), createEmployee(), createPerson(), uploadProduct() methods are done

--- Supermarket/src/com/codecool/Supermarket.java
+++ Supermarket/src/com/codecool/Supermarket.java
@@ -1,6 +1,10 @@
+package com.codecool;
+
 import java.util.*;
+import java.io.*;
 
 public class Supermarket {
+
     private String name;
     private Person[] persons;
     private Product[] products;
@@ -45,5 +49,66 @@ public class Supermarket {
         products = tempArray;
     }
 
-    public Product createProduct(Pro)
+    public Customer createCustomer(PersonType personType) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your full name?");
+        String customerFullName = sc.nextLine();
+        System.out.println("What is your gender?");
+        String customerGender = sc.nextLine();
+        System.out.println("How old are you?");
+        int customerAge = sc.nextInt();
+        System.out.println("How much money have you got?");
+        int customerMoney = sc.nextInt();
+        Customer person = new Customer(customerFullName, customerGender, customerAge, customerMoney, personType);
+        return person;
+    }
+
+    public Employee createEmployee(PersonType personType) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your full name?");
+        String employeeFullName = sc.nextLine();
+        System.out.println("What is your gender?");
+        String employeeGender = sc.nextLine();
+        System.out.println("How old are you?");
+        int employeeAge = sc.nextInt();
+        System.out.println("What is your position?");
+        String employeePosition = sc.nextLine();
+        Employee person = new Employee(employeeFullName, employeeGender, employeeAge, employeePosition, personType);
+        return person;
+    }
+
+    public void createPerson(PersonType personType) {
+        Person person;
+        switch(personType) {
+            case CUSTOMER:
+                person = createCustomer(personType);
+                break;
+            case EMPLOYEE:
+                person = createEmployee(personType);
+                break;
+            default:
+                throw new IllegalArgumentException("This person type is invalid!");
+        }
+        addToPerson(person);
+    }
+
+    public void uploadProduct(String csvFile) {
+        String line = "";
+        try(BufferedReader br = new BufferedReader(new FileReader("../data/"+csvFile))){
+            while((line = br.readLine()) != null){
+                String[] product = line.split(",");
+                if (product.length == 4) {
+                    NonEdibleProduct nonEdibleProduct = new NonEdibleProduct(product[0], Integer.parseInt(product[1]), Integer.parseInt(product[2]), product[3]);
+                    addToProduct(nonEdibleProduct);
+                } else if (product.length == 5) {
+                    EdibleProduct edibleProduct = new EdibleProduct(product[0], Integer.parseInt(product[1]), Integer.parseInt(product[2]), Integer.parseInt(product[3]), Boolean.parseBoolean(product[4]));
+                    addToProduct(edibleProduct);
+                }
+            }
+        }
+        catch(IOException e) {
+            System.out.println("There is no file to read.");
+            e.printStackTrace();
+        }
+    }
 }
