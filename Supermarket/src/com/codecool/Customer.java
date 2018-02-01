81c47d3df79e27fa6ef174c4f9e6dea01b79e12a
Bernáth Máté <bernath.mate.new@gmail.com>
2018-02-01 18:21:03 +0100

    shoppingCart field is deleted, constructor is modified, getShoppingCart() method is deleted, printShoppingCartContent() is modified, addToShoppingCart() method is deleted, costOfProducts() method is implemented

--- Supermarket/src/com/codecool/Customer.java
+++ Supermarket/src/com/codecool/Customer.java
@@ -5,57 +5,70 @@ import java.util.*;
 public class Customer extends Person {
 
     private int money;
-    private Product[] shoppingCart;
     private static final int SHOPPING_CART_CAPACITY = 15;
 
     public Customer(String fullName, String gender, int age, int money, PersonType personType) {
         super(fullName, gender, age, personType);
         this.money = money;
-        shoppingCart = new Product[0];
     }
 
     public int getMoney() {
         return money;
     }
 
-    public Product[] getShoppingCart() {
-        return shoppingCart;
+    public void decreaseMoney(int money) {
+        this.money -= money;
     }
 
     public void printShoppingCartContent() {
-        System.out.println("Shopping cart content: ");
-        for (int i = 0; i < shoppingCart.length; i++) {
-            System.out.println("\t" + shoppingCart[i]);
+        if (products.length > 0) {
+            System.out.println("Shopping cart content: ");
+            for (int i = 0; i < products.length; i++) {
+                System.out.println("\t" + products[i]);
+            }
+        } else {
+            System.out.println("Shopping cart is empty!");
         }
     }
     
-    public void addToShoppingCart(Product product) {
-        if (shoppingCart.length <= SHOPPING_CART_CAPACITY) {
-            Product[] tempArray = new Product[shoppingCart.length + 1];
-            for(int i = 0; i < shoppingCart.length; i++) {
-                tempArray[i] = shoppingCart[i];
-            }
-            tempArray[tempArray.length - 1] = product;
-            shoppingCart = tempArray;
+    @Override
+    public void addToProduct(Product product) {
+        if (products.length <= SHOPPING_CART_CAPACITY) {
+            super.addToProduct(product);
         } else {
             System.out.println("Your shopping cart's capacity is full!");
         }
     }
 
     public void deleteFromShoppingCart(Product product) {
-        if (shoppingCart.length > 0) {
-            Product[] tempArray = new Product[shoppingCart.length - 1];
+        if (products.length > 0) {
+            Product[] tempArray = new Product[products.length - 1];
             int tempVar = 0;
-            for(int i = 0; i < shoppingCart.length; i++) {
-                if(shoppingCart[i].getName().equals(product.getName())) {
+            for(int i = 0; i < products.length; i++) {
+                if(products[i].getName().equals(product.getName())) {
                     tempVar = 1;
                 } else {
-                    tempArray[i - tempVar] = shoppingCart[i]; 
+                    tempArray[i - tempVar] = products[i]; 
                 }
+            products = tempArray;
             }
         } else {
             System.out.println("You can't remove a product because your shopping cart is empty!");
         }
+    }
+
+    public int costOfProducts() {
+        int costOfProducts = 0;
+        if (products.length > 0) {
+            for (int i = 0; i < products.length; i++) {
+                int currentProductCost = products[i].getPrice() * products[i].getAmount();
+                costOfProducts += currentProductCost;
+            }
+            return costOfProducts;
+        } else {
+            System.out.println("Your shopping cart is empty!");
+        }
+        return 0;
     }
 
     @Override
