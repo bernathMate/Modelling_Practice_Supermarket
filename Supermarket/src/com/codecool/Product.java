d9ec0bb75ddc24175cb0ebffe7d4f3a7759ea9d5
Bernáth Máté <bernath.mate.new@gmail.com>
2018-02-01 11:11:49 +0100

    class is modified to abstract class, copyProduct() method is implemented, copy constructor is implemented, setAmount(), decreaseAmount(), increaseAmount() methods is added

--- Supermarket/src/com/codecool/Product.java
+++ Supermarket/src/com/codecool/Product.java
@@ -1,6 +1,15 @@
 package com.codecool;
 
-public class Product implements java.io.Serializable {
+public abstract class Product implements java.io.Serializable {
+
+    public static Product copyProduct(Product p) {
+        if (p instanceof EdibleProduct) {
+            return new EdibleProduct((EdibleProduct)p);
+        } else if (p instanceof NonEdibleProduct) {
+            return new NonEdibleProduct((NonEdibleProduct)p);
+        }
+        return null;
+    }
 
     private String name;
     private int price;
@@ -10,6 +19,12 @@ public class Product implements java.io.Serializable {
         this.name = name;
         this.price = price;
         this.amount = amount;
+    }
+
+    public Product(Product otherProduct) {
+        this.name = otherProduct.name;
+        this.price = otherProduct.price;
+        this.amount = otherProduct.amount;
     }
 
     public String getName() {
@@ -22,6 +37,18 @@ public class Product implements java.io.Serializable {
 
     public int getAmount() {
         return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public void decreaseAmount(int amount) {
+        this.amount -= amount;
+    }
+
+    public void increaseAmount(int amount) {
+        this.amount += amount;
     }
     
 }
